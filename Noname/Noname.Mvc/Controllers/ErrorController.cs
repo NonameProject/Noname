0b31f32fd3d54afc6dae19c56659bef29bef6991
Kotvitskiy <another0ne@mail.ru>
2015-07-19 15:17:28 +0400

    one more time...

---
+++ Noname/Noname.Mvc/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿using Elmah;
+using Noname.Mvc.Components.CustomExceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Noname.Mvc.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public void LogJavaScriptError(string message)
+        {
+            ErrorSignal
+                .FromCurrentContext()
+                .Raise(new JavaScriptException(message));
+        }
+    }
+}
