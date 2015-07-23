68718a54e4855475f7b3b6c78403def8c81bda36
BorysMiller <milbro1995@gmail.com>
2015-07-23 20:07:17 +0300

    NHibernateDataProvider added

---
+++ Noname/Abitcareer.NHibernateDataProvider/NHibernateCore/Helper.cs
@@ -0,0 +1,23 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+
+namespace Events.NHibernateDataProvider.NHibernateCore
+{
+    public class Helper
+    {
+        private static ISessionFactory sessionFactory;
+
+        public static ISession OpenSession()
+        {
+            if (sessionFactory == null)
+            {
+                var config = new Configuration();
+                var data = config.Configure();
+                config.AddAssembly(typeof(Helper).Assembly);
+                sessionFactory = data.BuildSessionFactory();
+            }
+
+            return sessionFactory.OpenSession();
+        }
+    }
+}
