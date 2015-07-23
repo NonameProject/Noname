438c4ae5ae073bc32745e979d382ff81c48e2f23
mrrbodia <02bodia20@ukr.net>
2015-07-23 03:13:05 +0300

    highcharts and miniprofiler

--- Noname/Abitcareer.Mvc/AbitcareerApplication.cs
+++ Noname/Abitcareer.Mvc/AbitcareerApplication.cs
@@ -14,6 +14,96 @@ using System.Web.Routing;
 
 namespace Abitcareer.Mvc
 {
+    public class ProfilingActionFilter : ActionFilterAttribute
+    {
+        const string stackKey = "ProfilingActionFilterStack";
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var mp = MiniProfiler.Current;
+            if (mp != null)
+            {
+                var stack = HttpContext.Current.Items[stackKey] as Stack<IDisposable>;
+                if (stack == null)
+                {
+                    stack = new Stack<IDisposable>();
+                    HttpContext.Current.Items[stackKey] = stack;
+                }
+
+                var prof = MiniProfiler.Current.Step("Controller: " + filterContext.Controller.ToString() + "." + filterContext.ActionDescriptor.ActionName);
+                stack.Push(prof);
+
+            }
+            base.OnActionExecuting(filterContext);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+            var stack = HttpContext.Current.Items[stackKey] as Stack<IDisposable>;
+            if (stack != null && stack.Count > 0)
+            {
+                stack.Pop().Dispose();
+            }
+        }
+    }
+
+    public class ProfilingViewEngine : IViewEngine
+    {
+        class WrappedView : IView
+        {
+            IView wrapped;
+            string name;
+            bool isPartial;
+
+            public WrappedView(IView wrapped, string name, bool isPartial)
+            {
+                this.wrapped = wrapped;
+                this.name = name;
+                this.isPartial = isPartial;
+            }
+
+            public void Render(ViewContext viewContext, System.IO.TextWriter writer)
+            {
+                using (MiniProfiler.Current.Step("Render " + (isPartial ? "partial" : "") + ": " + name))
+                {
+                    wrapped.Render(viewContext, writer);
+                }
+            }
+        }
+
+        IViewEngine wrapped;
+
+        public ProfilingViewEngine(IViewEngine wrapped)
+        {
+            this.wrapped = wrapped;
+        }
+
+        public ViewEngineResult FindPartialView(ControllerContext controllerContext, string partialViewName, bool useCache)
+        {
+            var found = wrapped.FindPartialView(controllerContext, partialViewName, useCache);
+            if (found != null && found.View != null)
+            {
+                found = new ViewEngineResult(new WrappedView(found.View, partialViewName, isPartial: true), this);
+            }
+            return found;
+        }
+
+        public ViewEngineResult FindView(ControllerContext controllerContext, string viewName, string masterName, bool useCache)
+        {
+            var found = wrapped.FindView(controllerContext, viewName, masterName, useCache);
+            if (found != null && found.View != null)
+            {
+                found = new ViewEngineResult(new WrappedView(found.View, viewName, isPartial: false), this);
+            }
+            return found;
+        }
+
+        public void ReleaseView(ControllerContext controllerContext, IView view)
+        {
+            wrapped.ReleaseView(controllerContext, view);
+        }
+    }
     public class AbitcareerApplication : System.Web.HttpApplication
     {
         private IHttpModule Module = new LocalizationHttpModule();
@@ -41,6 +131,18 @@ namespace Abitcareer.Mvc
         protected void Application_BeginRequest()
         {
             MiniProfiler.Start();
+            // Add Profiling Action Filter (mvc mini profiler)
+            GlobalFilters.Filters.Add(new ProfilingActionFilter());
+
+            // Add Profiling View Engine (mvc mini profiler)
+            var copy = ViewEngines.Engines.ToList();
+            ViewEngines.Engines.Clear();
+            foreach (var item in copy)
+            {
+                ViewEngines.Engines.Add(new ProfilingViewEngine(item));
+            }
+
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
         }
 
         protected void Application_EndRequest()
@@ -53,6 +155,26 @@ namespace Abitcareer.Mvc
         {
             var principal = httpRequest.RequestContext.HttpContext.User;
             return principal.IsInRole("admin");
+        }
+        public static bool DisableProfilingResults { get; set; }
+        private void InitProfilerSettings()
+        {
+            // by default, sql parameters won't be displayed
+            MiniProfiler.Settings.SqlFormatter = new StackExchange.Profiling.SqlFormatters.InlineFormatter();
+            MiniProfiler.Settings.ShowControls = false;
+            MiniProfiler.Settings.StackMaxLength = 256;
+            MiniProfiler.Settings.Results_Authorize = request =>
+            {
+                if ("/Home/ResultsAuthorization".Equals(request.Url.LocalPath, StringComparison.OrdinalIgnoreCase))
+                {
+                    return (request.Url.Query).ToLower().Contains("isauthorized");
+                }
+                return !DisableProfilingResults;
+            };
+            MiniProfiler.Settings.Results_List_Authorize = request =>
+            {
+                return true;
+            };
         }
 
         public override void Init()
--- Noname/Abitcareer.Web/Scripts/chartsScript.js
+++ Noname/Abitcareer.Web/Scripts/chartsScript.js
@@ -27,79 +27,187 @@
    
 
 
-    function drawCharts(conteiner, dataObj, title, xAxisCaption, yAxisCaption, valueTypes) {
-        $(conteiner).highcharts({
-            chart: {
-                type: 'spline',
-                inverted: true
-            },
+function drawCharts(conteiner, dataObj, title, xAxisCaption, yAxisCaption, valueTypes) {
+    renderTheme();
+    $(conteiner).highcharts({
+        chart: {
+            type: 'spline',
+            inverted: true,
+            zoomtype: "xy"
+        },
+        title: {
+            text: title
+        },
+        xAxis: {
+            reversed: false,
             title: {
-                text: title
+                enabled: true,
+                text: xAxisCaption
             },
-            xAxis: {
-                reversed: false,
-                title: {
-                    enabled: true,
-                    text: xAxisCaption
-                },
-                labels: {
-                    formatter: function () {
-                        return this.value;
-                    }
-                },
-                maxPadding: 0.1,
-                showLastLabel: true,
-                //gridLineWidth: 0.5
-                tickInterval: 1000
-            },
-            yAxis: {
-                title: {
-                    text: yAxisCaption
-                },
-                labels: {
-                    formatter: function () {
-                        return this.value;
-                    }
-                },
-                lineWidth: 1
-            },
-            legend: {
-                enabled: false
-            },
-            tooltip: {
-                headerFormat: '<b>{series.name}</b><br/>',
-                formatter: function()
-                {                   
-                    var header = "<strong>" + this.series.name +  "</strong><br/>";
-                    if (this.series.color == "green")
-                        return header+ valueTypes.profit +":{" + this.x + "}," +  valueTypes.year +  "{" + this.y + "}";
-                    return header + valueTypes.costs+ ":{" + this.x + "},"  +  valueTypes.year+ "{" + this.y + "}";
+            labels: {
+                formatter: function () {
+                    return this.value;
                 }
             },
-            plotOptions: {
-                spline: {
-                    marker: {
-                        enable: false
-                    }
+            maxPadding: 0.1,
+            showLastLabel: true,
+            //gridLineWidth: 0.5
+            tickInterval: 1000
+        },
+        yAxis: {
+            title: {
+                text: yAxisCaption
+            },
+            labels: {
+                formatter: function () {
+                    return this.value;
                 }
             },
-            series: dataObj
-        }, function(chart){
-            var s0 = chart.series[0].points;
-            var s1 = chart.series[1].points;
-            var s2 = chart.series[1];
-            var n0 = s0.length;
-            var n1 = s1.length;
-            var i,j,isect;
-            for (i = 1; i < n0; i++){
-                for (j = 1; j < n1; j++){
-                    if (isect = get_line_intersection(s0[i-1],s0[i],
-                                        s1[j-1],s1[j])){
-                        s2.addPoint(isect, false, false);
-
-                    }
-                } 
+            lineWidth: 1
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: {
+            headerFormat: '<b>{series.name}</b><br/>',
+            formatter: function()
+            {                   
+                var header = "<strong>" + this.series.name +  "</strong><br/>";
+                if (this.series.color == "green")
+                    return header+ valueTypes.profit +":{" + this.x + "}," +  valueTypes.year +  "{" + this.y + "}";
+                return header + valueTypes.costs+ ":{" + this.x + "},"  +  valueTypes.year+ "{" + this.y + "}";
             }
-            chart.redraw();
-        });
-    }
+        },
+        plotOptions: {
+            spline: {
+                marker: {
+                    enable: false
+                }
+            }
+        },
+        series: dataObj
+    }, function (chart) {
+        chart.setSize(
+       $('.chartWrapper').width(),
+       $('.chartWrapper').height(),
+       false
+    );
+        var s0 = chart.series[0].points;
+        var s1 = chart.series[1].points;
+        var s2 = chart.series[1];
+        var n0 = s0.length;
+        var n1 = s1.length;
+        var i,j,isect;
+        for (i = 1; i < n0; i++){
+            for (j = 1; j < n1; j++){
+                if (isect = get_line_intersection(s0[i-1],s0[i],
+                                    s1[j-1],s1[j])){
+                    s2.addPoint(isect, false, false);
+
+                }
+            } 
+        }
+        chart.redraw();
+    });
+}
+
+function renderTheme() {
+    Highcharts.createElement('link', {
+        href: '//fonts.googleapis.com/css?family=Signika:400,700',
+        rel: 'stylesheet',
+        type: 'text/css'
+    }, null, document.getElementsByTagName('head')[0]);
+
+    // Add the background image to the container
+    Highcharts.wrap(Highcharts.Chart.prototype, 'getContainer', function (proceed) {
+        proceed.call(this);
+        this.container.style.background = 'url(http://www.highcharts.com/samples/graphics/sand.png)';
+    });
+
+
+    Highcharts.theme = {
+        colors: ["#f45b5b", "#8085e9", "#8d4654", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
+           "#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
+        chart: {
+            backgroundColor: null,
+            style: {
+                fontFamily: "Signika, serif"
+            }
+        },
+        title: {
+            style: {
+                color: 'black',
+                fontSize: '16px',
+                fontWeight: 'bold'
+            }
+        },
+        subtitle: {
+            style: {
+                color: 'black'
+            }
+        },
+        tooltip: {
+            borderWidth: 0
+        },
+        legend: {
+            itemStyle: {
+                fontWeight: 'bold',
+                fontSize: '13px'
+            }
+        },
+        xAxis: {
+            labels: {
+                style: {
+                    color: '#6e6e70'
+                }
+            }
+        },
+        yAxis: {
+            labels: {
+                style: {
+                    color: '#6e6e70'
+                }
+            }
+        },
+        plotOptions: {
+            series: {
+                shadow: true
+            },
+            candlestick: {
+                lineColor: '#404048'
+            },
+            map: {
+                shadow: false
+            }
+        },
+
+        // Highstock specific
+        navigator: {
+            xAxis: {
+                gridLineColor: '#D0D0D8'
+            }
+        },
+        rangeSelector: {
+            buttonTheme: {
+                fill: 'white',
+                stroke: '#C0C0C8',
+                'stroke-width': 1,
+                states: {
+                    select: {
+                        fill: '#D0D0D8'
+                    }
+                }
+            }
+        },
+        scrollbar: {
+            trackBorderColor: '#C0C0C8'
+        },
+
+        // General
+        background2: '#E0E0E8'
+
+    };
+
+    // Apply the theme
+    Highcharts.setOptions(Highcharts.theme);
+}
