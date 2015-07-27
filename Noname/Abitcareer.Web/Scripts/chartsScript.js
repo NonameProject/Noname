8ddc508da2b6fe8156b27624e8580cb5abe3f3c2
Vlad Stepchin <vladmasterman@gmail.com>
2015-07-27 15:27:49 +0300

    two highcharts into one, bright colors, and so on)

--- Noname/Abitcareer.Web/Scripts/chartsScript.js
+++ Noname/Abitcareer.Web/Scripts/chartsScript.js
@@ -1,6 +1,6 @@
-﻿var chartHeight = 400;
-get_line_intersection = function (p0, p1, p2, p3)
-{        
+﻿
+var chartHeight = 400;
+get_line_intersection = function (p0, p1, p2, p3) {
     var p0_x = p0.x,
         p0_y = p0.y,
         p1_x = p1.x,
@@ -8,18 +8,17 @@ get_line_intersection = function (p0, p1, p2, p3)
         p2_x = p2.x,
         p2_y = p2.y,
         p3_x = p3.x,
-        p3_y = p3.y;  
+        p3_y = p3.y;
 
     var s1_x, s1_y, s2_x, s2_y;
-    s1_x = p1_x - p0_x;     s1_y = p1_y - p0_y;
-    s2_x = p3_x - p2_x;     s2_y = p3_y - p2_y;
+    s1_x = p1_x - p0_x; s1_y = p1_y - p0_y;
+    s2_x = p3_x - p2_x; s2_y = p3_y - p2_y;
 
     var s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y),
-        t = ( s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y);
+        t = (s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y);
 
-    if (s >= 0 && s <= 1 && t >= 0 && t <= 1)
-    {
-        return [p0_x + (t * s1_x),p0_y + (t * s1_y)];
+    if (s >= 0 && s <= 1 && t >= 0 && t <= 1) {
+        return [p0_x + (t * s1_x), p0_y + (t * s1_y)];
     }
 
     return false;
@@ -51,6 +50,7 @@ function drawCharts(conteiner, dataObj, title, xAxisCaption, yAxisCaption, dotCa
             tickInterval: 1000
         },
         yAxis: {
+            categories: ['0'],
             showEmpty: false,
             title: {
                 text: yAxisCaption
@@ -58,7 +58,6 @@ function drawCharts(conteiner, dataObj, title, xAxisCaption, yAxisCaption, dotCa
             labels: {
                 formatter: function () {
                     if (this.y === 0) {
-                        return null;
                     } else {
                         return this.value;
                     }
@@ -71,15 +70,14 @@ function drawCharts(conteiner, dataObj, title, xAxisCaption, yAxisCaption, dotCa
         },
         tooltip: {
             headerFormat: '<b>{series.name}</b><br/>',
-            formatter: function()
-            {
-               
-                var header = "<strong>" + this.series.name +  "</strong><br/>";
+            formatter: function () {
+
+                var header = "<strong>" + this.series.name + "</strong><br/>";
                 if (this.series.color == "green")
                     return header + valueTypes.profit + ":{" + this.x.toFixed(1) + "}," + valueTypes.year + "{" + this.y.toFixed(1) + "}";
                 /*temporary code*/
-                if (((this.x.toFixed(1) == 1500 || this.x.toFixed(1) == 900) && this.y.toFixed(1) == 2.3) || (this.x.toFixed(1) == 1200 && (this.y.toFixed(1) == 2.3 || this.y.toFixed(1) == 2)) || ((this.x.toFixed(1) == 960 || this.x.toFixed(1) == 720) && this.y.toFixed(1) == 2) || ((this.x.toFixed(1) == 654.5 || this.x.toFixed(1) == 872.7) && (this.y.toFixed(1) == 2.9)) || ((this.x.toFixed(1) == 1090.9) && (this.y.toFixed(1) == 2.9)))
-                   return dotCaption;
+                //if (((this.x.toFixed(1) == 1500 || this.x.toFixed(1) == 900) && this.y.toFixed(1) == 2.3) || (this.x.toFixed(1) == 1200 && (this.y.toFixed(1) == 2.3 || this.y.toFixed(1) == 2)) || ((this.x.toFixed(1) == 960 || this.x.toFixed(1) == 720) && this.y.toFixed(1) == 2) || ((this.x.toFixed(1) == 654.5 || this.x.toFixed(1) == 872.7) && (this.y.toFixed(1) == 2.9)) || ((this.x.toFixed(1) == 1090.9) && (this.y.toFixed(1) == 2.9)))
+                //  return dotCaption;
                 return header + valueTypes.costs + ":{" + this.x.toFixed(1) + "}," + valueTypes.year + "{" + this.y.toFixed(1) + "}";
             }
         },
@@ -97,8 +95,7 @@ function drawCharts(conteiner, dataObj, title, xAxisCaption, yAxisCaption, dotCa
        chartHeight,
        false
     );
-        if (chart.series[0].points.length != 0 || chart.series[1].points.length != 0)
-        {
+        if (chart.series[0].points.length != 0 || chart.series[1].points.length != 0) {
             var s0 = chart.series[0].points,
             s1 = chart.series[1].points,
             s2 = chart.series[1];
@@ -137,7 +134,7 @@ function drawCharts(conteiner, dataObj, title, xAxisCaption, yAxisCaption, dotCa
                 };
             }
         }
-        
+
 
         $(window).resize(function () {
             chart.redraw();
