8d6150260f4413c64dd821d05cf052d3364fa6b4
Arsendiabolikkys <diabolikkys@gmail.com>
2015-07-27 14:03:41 +0300

    ProfiledSqlCeServer

---
+++ Noname/Noname.Business/Components/Miniprofiler/HasManyConvention.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Conventions;
+using FluentNHibernate.Conventions.Instances;
+
+namespace Abitcareer.Business.Components.Miniprofiler
+{
+    class HasManyConvention : IHasManyConvention
+    {
+        public void Apply(IOneToManyCollectionInstance instance)
+        {
+            instance.Key.Column(instance.EntityType.Name + "Fk");
+
+            //This line is added as new
+            instance.BatchSize(5);
+
+            instance.Cascade.All();
+        }
+    }
+}
