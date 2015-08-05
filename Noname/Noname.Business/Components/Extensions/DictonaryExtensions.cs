f50ad7647feaffe2f272e9abada3d1c8c7d74428
Vlad Stepchin <vladmasterman@gmail.com>
2015-08-05 14:47:46 +0300

    add some extensions

---
+++ Noname/Noname.Business/Components/Extensions/DictonaryExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Abitcareer.Business.Components.Extensions
+{
+    public static class DictonaryExtensions
+    {
+        public static string ToXmlString<TKey, TValue>(this Dictionary<TKey, TValue> dictionary)
+        {
+            return new XElement("fields", dictionary
+                        .Select(x => new XElement("field", new XAttribute("key", x.Key), new XAttribute("value", x.Value))))
+                        .ToString();
+        }
+    }
+}
---
+++ Noname/Noname.Business/Components/Extensions/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Abitcareer.Business.Components.Extensions
+{
+    public static class StringExtensions
+    {
+        public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(this string str)
+        {
+            var tempDict = XElement
+                    .Parse(str)
+                    .Descendants("field")
+                    .ToDictionary(x => x.Attribute("key").ToString(), x => x.Attribute("value").ToString());
+            var result = new Dictionary<TKey, TValue>();
+            foreach (var item in tempDict)
+	        {
+                result[(TKey)Convert.ChangeType(item.Key, typeof(TKey))] = (TValue)Convert.ChangeType(item.Value, typeof(TValue));		 
+	        }
+            return result;
+        }
+    }
+}
