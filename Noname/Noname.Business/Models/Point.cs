15a90d58908f6e3cf8ae2b392c3e75c2e2fde672
BilyachenkoOY <iviviv1901@gmail.com>
2015-08-06 20:25:18 +0300

    Aproximation

---
+++ Noname/Noname.Business/Models/Point.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+namespace Abitcareer.Business.Models
+{
+    public class Point
+    {
+        public Point(double x, double y)
+        {
+            this.x = (int)Math.Round(x);
+            this.y = (int)Math.Round(y);
+        }
+
+        public int x;
+
+        public int y;
+    }
+}
