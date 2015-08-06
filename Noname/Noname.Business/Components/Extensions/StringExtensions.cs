5e900d9a57e66e82b6e86d8c4c379e8ddbaadfe3
BilyachenkoOY <iviviv1901@gmail.com>
2015-08-06 23:07:59 +0300

    check str for null

--- Noname/Noname.Business/Components/Extensions/StringExtensions.cs
+++ Noname/Noname.Business/Components/Extensions/StringExtensions.cs
@@ -10,12 +10,14 @@ namespace Abitcareer.Business.Components.Extensions
     public static class StringExtensions
     {
         public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(this string str)
-        {
-            var tempDict = XElement
-                    .Parse(str)
-                    .Descendants("field")
-                    .ToDictionary(x => x.Attribute("key").Value, x => x.Attribute("value").Value);
+        {          
             var result = new Dictionary<TKey, TValue>();
+            if(string.IsNullOrEmpty(str))
+                return result;
+            var tempDict = XElement
+                        .Parse(str)
+                        .Descendants("field")
+                        .ToDictionary(x => x.Attribute("key").Value, x => x.Attribute("value").Value);
             foreach (var item in tempDict)
 	        {
                 result[(TKey)Convert.ChangeType(item.Key, typeof(TKey))] = (TValue)Convert.ChangeType(item.Value, typeof(TValue));		 
