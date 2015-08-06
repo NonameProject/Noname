eb3d31f3f65768a28ab52c24f19349b8d7831c29
BilyachenkoOY <iviviv1901@gmail.com>
2015-08-06 20:25:52 +0300

    test getData

--- Noname/Abitcareer.Mvc/Controllers/HomeController.cs
+++ Noname/Abitcareer.Mvc/Controllers/HomeController.cs
@@ -1,15 +1,23 @@
-﻿using Abitcareer.Business.Models;
-using Abitcareer.NHibernateDataProvider.Data_Providers;
-using System.Collections.Generic;
+﻿using Abitcareer.Business.Components;
 using System.Web.Mvc;
+using System.Collections.Generic;
 
 namespace Abitcareer.Mvc.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
+        {            
             return View();
+        }
+
+        public ActionResult GetData(short polinom)
+        {
+            var x = new double[] { 2, 3, 4, 5};
+            var y = new double[] { 800, 3000, 5000, 7000 };
+            var aproximator = new Approximator(x, y, polinom);
+            x = new double[] { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            return Json(aproximator.CalcY(new List<double>(x)), JsonRequestBehavior.AllowGet);
         }
     }
 }
