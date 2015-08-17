248c194b7905f24e82f0c26de0684e7f07209a73
BilyachenkoOY <iviviv1901@gmail.com>
2015-08-17 13:19:26 +0300

    #fed highlighting input after search

--- Noname/Abitcareer.Web/Scripts/search.js
+++ Noname/Abitcareer.Web/Scripts/search.js
@@ -1,6 +1,7 @@
 ﻿var id =0;
 $("#search").keydown(function () {
     clearTimeout(id);
+    $(this).removeClass('success', 100);
     id = setTimeout(function () {
         var value = $('#search').val();
         if (!value && !value.trim()) {
@@ -11,8 +12,9 @@ $("#search").keydown(function () {
             {
                 url: "searchforspeaciality",
                 type: "POST",
-                data: { name: $("#search").val() },
+                data: { name: value },
                 success: function (result) {
+                    $('#search').addClass('success');
                     $(".cardWrapper:not(#addCard)").hide(0);
                     for (var i = 0; i < result.length; i++) {
                         $("#" + result[i]).show(0);
