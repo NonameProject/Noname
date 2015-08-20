958208c647c90106ef55e428317f4fbf3ca7a632
AlexSolari <alexsanify@gmail.com>
2015-08-20 13:44:38 +0300

    Message about disabled cookies

---
+++ Noname/Abitcareer.Web/Scripts/Login.js
@@ -0,0 +1,16 @@
+﻿function CookiesEnabled() {
+    var cookieEnabled = (navigator.cookieEnabled) ? true : false;
+
+    if (typeof navigator.cookieEnabled == "undefined" && !cookieEnabled) {
+        document.cookie = "testcookie";
+        cookieEnabled = (document.cookie.indexOf("testcookie") != -1) ? true : false;
+    }
+    return (cookieEnabled);
+}
+
+$(function () {
+    if (!CookiesEnabled())
+    {
+        $(".cookies-off-wrapper").show();
+    }
+});
