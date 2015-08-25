b57e2fa1732eda3d1179c2d8d4bc14f8b2dab141
mrrbodia <02bodia20@ukr.net>
2015-08-25 12:11:07 +0300

    refactoring revertZero js

--- Noname/Abitcareer.Web/Scripts/SpecialitiesEditor.js
+++ Noname/Abitcareer.Web/Scripts/SpecialitiesEditor.js
@@ -169,19 +169,6 @@
         settings.editor.children('input').prop('maxlength', 300);
     }
 
-    var revertZeroSubmit = function () {
-        var input = $('.salaries[value]')
-        var id = [1, 2, 3, 4, 5, 10, 20];
-        for (var i = 0; i < id.length; i++) {
-            if ($('#Salaries__' + id[i] + '_').val() == '') {
-                $('#Salaries__' + id[i] + '_').css('color', 'transparent').val(-1);
-            }
-        };
-        if ($('#Prices__TopUniversityPrice_').val() == '') $('#Prices__TopUniversityPrice_').css('color', 'transparent').val('-1');
-        if ($('#Prices__MiddleUniversityPrice_').val() == '') $('#Prices__MiddleUniversityPrice_').css('color', 'transparent').val('-1');
-        if ($('#Prices__LowUniversityPrice_').val() == '') $('#Prices__LowUniversityPrice_').css('color', 'transparent').val('-1');
-    };
-
     var isSpecialityNameAvailable = function()
     {
         if ($('#Name').val().length == 0) {
@@ -288,20 +275,6 @@
     return {
 
         resizeCards: resizeCards,
-
-        revertZero: function () {
-            var input = $('.salaries[value]')
-            var id = [1, 2, 3, 4, 5, 10, 20];
-            for (var i = 0; i < id.length; i++) {
-                if ($('#Salaries__' + id[i] + '_').val() == '-1') {
-                    $('#Salaries__' + id[i] + '_').val('');
-                }
-            };
-            if ($('#Prices__TopUniversityPrice_').val() == '-1') $('#Prices__TopUniversityPrice_').val('');
-            if ($('#Prices__MiddleUniversityPrice_').val() == '-1') $('#Prices__MiddleUniversityPrice_').val('');
-            if ($('#Prices__LowUniversityPrice_').val() == '-1') $('#Prices__LowUniversityPrice_').val('');
-            if ($('#StartOfWorking').val() == '0') $('#StartOfWorking').val('');
-        },
 
         getCulture: function () {
             return document.location.href.split('/')[3].toLowerCase();
---
+++ Noname/Abitcareer.Web/Scripts/discardZero.js
@@ -0,0 +1,26 @@
+﻿revertZero = function () {
+    var input = $('.salaries[value]')
+    var id = [1, 2, 3, 4, 5, 10, 20];
+    for (var i = 0; i < id.length; i++) {
+        if ($('#Salaries__' + id[i] + '_').val() == '-1') {
+            $('#Salaries__' + id[i] + '_').val('');
+        }
+    };
+    if ($('#Prices__TopUniversityPrice_').val() == '-1') $('#Prices__TopUniversityPrice_').val('');
+    if ($('#Prices__MiddleUniversityPrice_').val() == '-1') $('#Prices__MiddleUniversityPrice_').val('');
+    if ($('#Prices__LowUniversityPrice_').val() == '-1') $('#Prices__LowUniversityPrice_').val('');
+    if ($('#StartOfWorking').val() == '0') $('#StartOfWorking').val('');
+}
+
+var revertZeroSubmit = function () {
+    var input = $('.salaries[value]')
+    var id = [1, 2, 3, 4, 5, 10, 20];
+    for (var i = 0; i < id.length; i++) {
+        if ($('#Salaries__' + id[i] + '_').val() == '') {
+            $('#Salaries__' + id[i] + '_').css('color', 'transparent').val(-1);
+        }
+    };
+    if ($('#Prices__TopUniversityPrice_').val() == '') $('#Prices__TopUniversityPrice_').css('color', 'transparent').val('-1');
+    if ($('#Prices__MiddleUniversityPrice_').val() == '') $('#Prices__MiddleUniversityPrice_').css('color', 'transparent').val('-1');
+    if ($('#Prices__LowUniversityPrice_').val() == '') $('#Prices__LowUniversityPrice_').css('color', 'transparent').val('-1');
+};
