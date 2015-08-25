95d186345f65dbd12d24dcbda4877b173d50b1e7
mrrbodia <02bodia20@ukr.net>
2015-08-25 04:35:33 +0300

    з22: save zero

--- Noname/Abitcareer.Web/Scripts/SpecialitiesEditor.js
+++ Noname/Abitcareer.Web/Scripts/SpecialitiesEditor.js
@@ -173,8 +173,8 @@
         var input = $('.salaries[value]')
         var id = [1, 2, 3, 4, 5, 10, 20];
         for (var i = 0; i < id.length; i++) {
-            if ($('#Salaries_' + id[i] + '_').val() == '') {
-                $('#Salaries_' + id[i] + '_').css('color', 'transparent').val('0');
+            if ($('#Salaries__' + id[i] + '_').val() == '') {
+                $('#Salaries__' + id[i] + '_').css('color', 'transparent').val(-1);
             }
         };
         if ($('#Prices__TopUniversityPrice_').val() == '') $('#Prices__TopUniversityPrice_').css('color', 'transparent').val('0');
@@ -220,9 +220,10 @@
     var bindEditionFormSubmiting = function()
     {
         settings.editor.submit(function (event) {
-
             if (!isSpecialityNameAvailable() || !isSalaryValuesValid() || !isPriceValuesValid())
                 return;
+
+            revertZeroSubmit();
 
             var data = settings.editor.serialize(),
                 url = settings.editor.attr("action"),
@@ -233,8 +234,6 @@
             if (!settings.editor.hasClass('addForm')) {
                 specialityName = "[" + $('#' + id).find('div').find('.name').html().replace(new RegExp("\n", 'g'), "").replace(new RegExp(" ", 'g'), "") + '] - ';
             }
-
-            revertZeroSubmit();
 
             $.post(url, data, function (d) {
                 if (d) {
@@ -289,6 +288,20 @@
     return {
 
         resizeCards: resizeCards,
+
+        revertZero: function () {
+            var input = $('.salaries[value]')
+            var id = [1, 2, 3, 4, 5, 10, 20];
+            for (var i = 0; i < id.length; i++) {
+                if ($('#Salaries__' + id[i] + '_').val() == '-1') {
+                    $('#Salaries__' + id[i] + '_').val('');
+                }
+            };
+            if ($('#Prices__TopUniversityPrice_').val() == '0') $('#Prices__TopUniversityPrice_').val('');
+            if ($('#Prices__MiddleUniversityPrice_').val() == '0') $('#Prices__MiddleUniversityPrice_').val('');
+            if ($('#Prices__LowUniversityPrice_').val() == '0') $('#Prices__LowUniversityPrice_').val('');
+            if ($('#StartOfWorking').val() == '0') $('#StartOfWorking').val('');
+        },
 
         getCulture: function () {
             return document.location.href.split('/')[3].toLowerCase();
